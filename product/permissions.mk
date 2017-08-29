0dc645d4efbaa7c3199507dc1ecccd3709f4c82e
Jesse Hall <jessehall@google.com>
2017-08-29 15:46:29 -0700

    exynos7870-common: Declare Vulkan compute level 0 system feature
    Bug: 38396207 (VK_KHR_variable_pointers)
    Bug: 64982680 (maxPerStageDescriptorStorageBuffers >= 16)
    Test: CtsGraphicsTestCases:.VulkanFeaturesTest (ag/2810565)
    Change-Id: I61cde61f53f0e9a1c657e6bd6658a4393360b0a6

--- product/permissions.mk
+++ product/permissions.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
