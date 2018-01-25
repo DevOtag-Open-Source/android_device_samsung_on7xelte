67c8b98f6ab13c10a5721f61ba0b00c76dccf7d6
DarkLord1731 <darklord1731@gmail.com>
2018-01-25 21:54:13 +0530

    exynos7870-common: Add DRM and RenderScript HAL packages

--- product/graphics.mk
+++ product/graphics.mk
@@ -25,3 +25,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
