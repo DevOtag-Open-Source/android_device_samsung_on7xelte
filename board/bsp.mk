bef5497351ec3261fd2f87552d3b9f59d53f6dc1
DarkLord1731 <darklord1731@gmail.com>
2018-02-19 13:10:25 +0530

    exynos7870-common: Configure flags for Samsung BSP

---
+++ board/bsp.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# HDMI
+BOARD_HDMI_INCAPABLE := true
+
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+
+# (G)SCALER
+BOARD_USES_SCALER := true
+
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_QOS_CTRL := false
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_VP8ENC_SUPPORT := true
+
+# Video scaling issue workaround
+TARGET_OMX_LEGACY_RESCALING := true
