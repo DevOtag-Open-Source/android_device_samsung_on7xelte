1663df6aa9d8ebf2d8c99bd7175da246e0ae2c72
DarkLord1731 <darklord1731@gmail.com>
2018-02-19 20:38:10 +0530

    exynos7870-common: Build GPS HAL for O

--- product/gps.mk
+++ product/gps.mk
@@ -17,3 +17,6 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.cfg:system/etc/gps.cfg \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf
+
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
