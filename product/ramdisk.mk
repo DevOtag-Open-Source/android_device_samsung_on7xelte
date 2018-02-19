b11d68edb38894d993636a479b72e13aea30c0b4
DarkLord1731 <darklord1731@gmail.com>
2018-02-19 13:27:38 +0530

    exynos7870-common: Configure ramdisk files

---
+++ product/ramdisk.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.samsungexynos7870 \
+    init.baseband.rc \
+    init.rilchip.rc \
+    init.samsung.rc \
+    init.samsungexynos7870.rc \
+    init.samsungexynos7870.usb.rc \
+    ueventd.samsungexynos7870.rc
+
+# cpboot-daemon
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/cbd:root/sbin/cbd
+
+# sswap
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/sswap:root/sbin/sswap
