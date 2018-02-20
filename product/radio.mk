e90c57524f8da77d20eb0d2c2de98226f550b835
DarkLord1731 <darklord1731@gmail.com>
2018-02-20 08:18:43 +0530

    exynos7870-common: Add radio packages for O

--- product/radio.mk
+++ product/radio.mk
@@ -19,7 +19,12 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libsecril-client \
     libsecril-client-sap \
-    modemloader
+    modemloader\
+    rild \
+    libreference-ril \
+    libril \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.deprecated@1.0
 
 # IPv6
 PRODUCT_PACKAGES += \
