b894c3804f830688b09e7a7fc4ba1a4ed9b89fce
DarkLord1731 <darklord1731@gmail.com>
2018-04-05 10:29:40 +0530

    exynos7870-common: Remove HWC2 HIDL

--- product/graphics.mk
+++ product/graphics.mk
@@ -22,7 +22,6 @@ PRODUCT_PACKAGES += \
     libhwc2on1adapter \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
 # RenderScript HAL
