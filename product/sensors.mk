963b4b7aaf63755ab9b71553ade111425193ae90
Matt Grf <g.matthieu49@gmail.com>
2018-08-06 15:59:28 +0200

    exynos7870-common: Decommonise android.hardware.sensor@1.0-service
    This hidl broke sensors on a3 2017

--- product/sensors.mk
+++ product/sensors.mk
@@ -16,6 +16,5 @@
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     android.hardware.vibrator@1.0-service \
     android.hardware.vibrator@1.0-impl
