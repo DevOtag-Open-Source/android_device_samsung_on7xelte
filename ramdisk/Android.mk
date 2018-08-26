dc959c233d4b189e34a92e765867094570a0de59
DarkLord1731 <siddhantnaik17@gmail.com>
2018-08-26 21:24:23 +0530

    exynos7870-common: Cleanup ramdisk/Android.mk

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -39,14 +39,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.samsung.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
-LOCAL_SRC_FILES    := init.samsung.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := BOOT
@@ -67,14 +59,6 @@ LOCAL_MODULE       := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.service.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
-LOCAL_SRC_FILES    := init.service.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
