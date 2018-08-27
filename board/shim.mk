Exynos7870/android_device_samsung_exynos7870-common

exynos7870-common: Move shim lib to vendor

board/shim.mk
@@ -15,5 +15,5 @@
 
 # Shim
 TARGET_LD_SHIM_LIBS += \
-    /system/lib/libexynoscamera.so|/system/vendor/lib/libcamhelpr.so \
-    /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
+    /system/lib/libexynoscamera.so|/vendor/lib/libcamhelpr.so \
+    /system/lib/libstagefright.so|/vendor/lib/libstagefright_shim.so

DevOtag-Open-Source/android_device_samsung_on7xelte

board/shim.mk
@@ -15,4 +15,4 @@
 
 # Shim
 TARGET_LD_SHIM_LIBS += \
-    /system/lib/libexynoscamera.so|/system/vendor/lib/libcamhelpr.so
+    /system/lib/libexynoscamera.so|/vendor/lib/libcamhelpr.so