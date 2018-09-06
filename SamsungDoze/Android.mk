c3d0e2518f82f391339d911489961c641cdaa0bb
Lukas0610 <mail@lukasberger.at>
2018-09-06 10:59:44 +0530

    exynos7870:common: doze: Define SDK version

--- SamsungDoze/Android.mk
+++ SamsungDoze/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
 
 LOCAL_USE_AAPT2 := true
 
