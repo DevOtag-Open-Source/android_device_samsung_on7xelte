048bbd8815d8855d87dcc0e20aea58c42f9999b3
DarkLord1731 <siddhantnaik17@gmail.com>
2018-09-06 11:18:27 +0530

    exynos7870:common: Enable HWC2on1 adapter

--- product/graphics.mk
+++ product/graphics.mk
@@ -21,6 +21,7 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
 # RenderScript HAL
