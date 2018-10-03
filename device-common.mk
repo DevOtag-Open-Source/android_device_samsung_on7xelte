b663456d122d5209fb0f518ddbe993a7bfed607a
Shivam Jha <jhashivam2020@gmail.com>
2018-10-03 23:32:47 +0530

    exynos7870-common: Flag inheritance of closed source BSP

--- device-common.mk
+++ device-common.mk
@@ -23,5 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 -include $(LOCAL_PATH)/product/*.mk
 
 # Call Samsung LSI Board Support Package
+ifneq ($(INCLUDE_EXYNOS_BSP),)
 $(call inherit-product, hardware/samsung_slsi/exynos5/exynos5.mk)
 $(call inherit-product, hardware/samsung_slsi/exynos7870/exynos7870.mk)
+endif
