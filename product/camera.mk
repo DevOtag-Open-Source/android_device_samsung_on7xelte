aed824228c013494aa3b4b26df0f6a1484805442
DarkLord1731 <siddhantnaik17@gmail.com>
2019-03-24 20:18:37 +0530

    Revert "exynos7870-common: Import customized"
    This reverts commit cf3f9cebffc38490707c5336b3bcc13893e55cb1.

--- product/camera.mk
+++ product/camera.mk
@@ -19,5 +19,7 @@
     libexynoscamera_shim \
     libstagefright_shim \
     libcamera_client_shim \
-    android.hardware.camera.provider@2.4-impl.exynos7870 \
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
