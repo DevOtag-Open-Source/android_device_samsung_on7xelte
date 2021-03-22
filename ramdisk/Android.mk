a045ef5422c97da97dfc29aef2a1178b00643519
Abhishek Arpure <aarpure@codeaurora.org>
2021-03-22 10:48:55 +0000

    on7xelte: ramdisk: Move ueventd.samsungexynos7870.rc to /vendor
    Signed-off-by: trkzmn <trkzmn89@gmail.com>

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -80,8 +80,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7870.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
