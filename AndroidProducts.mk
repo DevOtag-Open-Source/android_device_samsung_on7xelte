e2676f8f39b00d08d2578086029d0980189d14d6
trkzmn <trkzmn89@gmail.com>
2021-03-22 09:54:24 +0000

    on7xelte: Use COMMON_LUNCH_CHOICES instead of add_lunch_combo
    Signed-off-by: trkzmn <trkzmn89@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_on7xelte.mk
+
+COMMON_LUNCH_CHOICES += \
+    lineage_on7xelte-user \
+    lineage_on7xelte-userdebug \
+    lineage_on7xelte-eng
--- vendorsetup.sh
+++
@@ -1,1 +0,0 @@
-add_lunch_combo lineage_on7xelte-userdebug
