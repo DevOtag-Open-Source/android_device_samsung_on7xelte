b77a732eb528a23c9b825a0ad89b6a86cb8a16e6
trkzmn <trkzmn89@gmail.com>
2021-03-22 10:59:46 +0000

    on7xelte: ramdisk: Fix deprecated module classes
    * build/make/core/prebuilt_internal.mk:57: error: fstab.samsungexynos7870 :
    unexpected LOCAL_MODULE_CLASS for prebuilts: BOOT.
    
    Signed-off-by: trkzmn <trkzmn89@gmail.com>

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.samsungexynos7870
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.samsungexynos7870
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -25,7 +25,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.baseband.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.baseband.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -33,7 +33,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.power.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -41,7 +41,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.rilchip.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.rilchip.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -49,7 +49,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -57,7 +57,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7870.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -65,7 +65,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -73,7 +73,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.wifi.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.wifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
@@ -81,7 +81,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
