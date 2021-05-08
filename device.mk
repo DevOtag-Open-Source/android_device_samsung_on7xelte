c11775dd68617e84e2012d4a74ad18c7d424a1ba
trkzmn <trkzmn89@gmail.com>
2021-05-08 02:43:37 +0000

    on7xelte: Switch to 2.1 hwcomposer hal
    Signed-off-by: trkzmn <trkzmn89@gmail.com>

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
